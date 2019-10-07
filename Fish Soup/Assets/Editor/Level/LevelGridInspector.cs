192dd13a5fcea3174a18b3197a60649c59bf4362
Super-Lovers <n.v.ivanov@st.hanze.nl>
2019-10-07 09:57:58 +0200

    Made readonly view for the grid scriptable object

---
+++ Fish Soup/Assets/Editor/Level/LevelGridInspector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(LevelGridView))]
+public class LevelGridInspector : Editor
+{
+    private LevelGridView targetScript;
+
+    private void OnEnable()
+    {
+        targetScript = (LevelGridView)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("In order to change these values, refer to \"Default Level Setting\" in the Config folder.", MessageType.Info);
+        EditorGUILayout.LabelField("Biome: " + targetScript.levelSettings.biome, EditorStyles.boldLabel);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Grid Size: (" +
+            "X: " + targetScript.levelSettings.gridSize.x + ", " +
+            "Y: " + targetScript.levelSettings.gridSize.y + ", " +
+            "Z: " + targetScript.levelSettings.gridSize.z + ")");
+
+        EditorGUILayout.LabelField("Tile Size: (" +
+            "X: " + targetScript.levelSettings.tileSize.x + ", " +
+            "Y: " + targetScript.levelSettings.tileSize.y + ", " +
+            "Z: " + targetScript.levelSettings.tileSize.z + ")");
+    }
+}
