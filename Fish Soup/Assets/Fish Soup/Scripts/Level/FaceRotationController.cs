ab5f7039b5c63e40493f86fa590d8c1e395c7ebb
Super-Lovers <n.v.ivanov@st.hanze.nl>
2019-10-14 22:51:47 +0200

    Made props to automatically rotate in editor mode

---
+++ Fish Soup/Assets/Fish Soup/Scripts/Level/FaceRotationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class FaceRotationController : MonoBehaviour
+{
+    public void RotateView()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, 100);
+    }
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            RotateView();
+        }
+    }
+}
