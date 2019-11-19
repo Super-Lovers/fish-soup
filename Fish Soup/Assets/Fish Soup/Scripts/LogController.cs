c79b2f3884809ba378387322b7b25e9442f0711b
Super-Lovers <n.v.ivanov@st.hanze.nl>
2019-11-19 23:00:27 +0100

    Created log controller + imported debug plugin

---
+++ Fish Soup/Assets/Fish Soup/Scripts/LogController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class LogController : MonoBehaviour
+{
+    [Tooltip("All game settings configurations are located in the \"Fish Soup/Config\" folder")]
+    [SerializeField] private GameSettingsModel gameSettingsModel = null;
+
+    /// <summary>
+    /// Settings configuration extracted from a non-serializable Game 
+    /// Settings Model in order to have one as a field for readability.
+    /// </summary>
+    private static GameSettingsModel gameSettingsConfig = null;
+
+    private void Awake()
+    {
+        gameSettingsConfig = new GameSettingsModel();
+        gameSettingsConfig.DebugMode = gameSettingsModel.DebugMode;
+
+        Toggle(gameSettingsConfig.DebugMode);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            gameSettingsConfig.DebugMode = !gameSettingsConfig.DebugMode;
+            Toggle(gameSettingsConfig.DebugMode);
+        }
+    }
+
+    public static void LogMessage(string message)
+    {
+        if (gameSettingsConfig.DebugMode == true)
+        {
+            Debug.Log(message);
+        }
+    }
+
+    /// <summary>
+    /// Toggles the debugging interface depending on the parameter given.
+    /// </summary>
+    private void Toggle(bool value)
+    {
+        gameSettingsConfig.DebugMode = value;
+        transform.GetChild(0).gameObject.SetActive(value);
+    }
+}
---
+++ Fish Soup/Assets/Fish Soup/Scripts/Scriptable Objects/GameSettingsModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Settings Model", menuName = "Scriptable Objects/Game Settings Model", order = 1)]
+public class GameSettingsModel : ScriptableObject
+{
+    /// <summary>
+    /// Toggles whether or not the debug console and it's messages will be
+    /// visualized in-game.
+    /// </summary>
+    public bool DebugMode = true;
+}
